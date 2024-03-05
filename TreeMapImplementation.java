3965b802ee95069d2665b5a93bf92fec09a6de85
GerardoFernandez7 <101151869+GerardoFernandez7@users.noreply.github.com>
2024-03-04 18:19:44 -0600

    Se implemento el metodo de leer archivo y agregar carta de la clase TreeMap

--- TreeMapImplementation.java
+++ TreeMapImplementation.java
@@ -1,9 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class TreeMapImplementation extends AbstractMap{
 
     public TreeMapImplementation(){
 
     }
 
+    public static Map<String, Carta> treeMapTXT = new TreeMap<>();
+    public static Map<String, Carta> coleccionUsuario = new TreeMap<>();
 
     /**
      * Lee información relevante desde un archivo.
@@ -12,13 +20,48 @@ public class TreeMapImplementation extends AbstractMap{
      */
     @Override
     void leerArchivo(String nombreArchivo) {
+        try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                // Dividir la línea en nombre y tipo usando el separador "|"
+                String[] partes = linea.split("\\|");
 
+                if (partes.length == 2) {
+                    String nombreCarta = partes[0].trim();
+                    String tipoCarta = partes[1].trim();
+
+                    Carta carta = new Carta(tipoCarta);
+
+                    // Almacenar en el HashMap
+                    treeMapTXT.put(nombreCarta, carta);
+                } else {
+                    System.out.println("Error en el formato de la línea: " + linea);
+                }
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     void agregarCarta() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'agregarCarta'");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa el nombre de la carta que deseas agregar a su colección: ");
+        String nombreCarta = sc.nextLine();
+        if (treeMapTXT.get(nombreCarta) != null){
+            Carta carta = new Carta(treeMapTXT.get(nombreCarta).getTipoCarta());
+            if (coleccionUsuario.containsKey(nombreCarta)){
+                int cantidad = coleccionUsuario.get(nombreCarta).getCantidad() + 1;
+                carta.setCantidad(cantidad);
+                coleccionUsuario.put(nombreCarta, carta);
+            }else {
+                carta.setCantidad(1);
+                coleccionUsuario.put(nombreCarta, carta);
+            }
+            System.out.println("Carta agregada correctamente.");
+        }else{
+            System.out.println("¡Carta no disponible!");
+        }
     }
 
     @Override
